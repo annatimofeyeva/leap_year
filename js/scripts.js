e85bbc66ec4c0fc1a4103ca78e9e89ff40a89dd9
Anna Timofeeva and Michael Dunlap <anna+michael@>
2017-06-07 10:42:16 -0700

    add refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 //business logic
 
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 100 !== 0) && (year % 4 === 0)) {
     return true;
   } else {
     return false;
   }
-
 };
 
 //user interface logic
